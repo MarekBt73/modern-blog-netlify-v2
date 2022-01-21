e8c85a584bb3bf64bddd29d5689e743a48d96750
MarekBecht <marek.becht@gmail.com>
2022-01-21 16:28:45 +0100

    nezt change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "ModrenB2C - servis stron www",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Serwis stron www - obsługa, uruchomienie, modernizacja wdrożenie stron i sklepów internetowych",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,12 +5,15 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import OfertPagePreview from './preview-templates/OfertPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('ofert', OfertPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
